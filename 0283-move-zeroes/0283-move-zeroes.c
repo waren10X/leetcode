343db4353d66231ea2ee31cc5f4093246291c029
houari mokrani <waren10@proton.me>
2024-10-28 18:32:06 +0100

    Time: 0 ms (100.00%), Space: 19.6 MB (47.22%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+void moveZeroes(int* nums, int numsSize)
+{
+    int i;
+    int j;
+
+    i = 0;
+    j = 0;
+    for (i = 0; i < numsSize; i++)
+    {
+        if (nums[i] != 0)
+        {
+            nums[j] = nums[i];
+            j++;
+        }
+    }
+    for (i = j; i < numsSize; i++)
+    {
+        nums[i] = 0;
+    }
+}
