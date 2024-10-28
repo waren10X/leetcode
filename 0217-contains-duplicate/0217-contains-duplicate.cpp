905137f75213a2bce0cee5199f8a4c7b84ae034b
houari mokrani <waren10@proton.me>
2024-10-28 19:02:37 +0100

    Time: 28 ms (72.01%), Space: 73.4 MB (49.09%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+#include <unordered_set>
+using namespace std;
+
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums)
+    {
+        unordered_set<int> seen;
+        for (int num : nums)
+        {
+            if (seen.count(num) > 0)
+            {
+                return true;
+            }
+            seen.insert(num);
+        }
+        return false;
+    }
+};
